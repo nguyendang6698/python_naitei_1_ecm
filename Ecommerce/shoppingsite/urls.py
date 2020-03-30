f291a3526bbbd0a51914f362d705c9ca69379d47
nguyendang6698 <nguyendang6698@github.com>
2020-03-30 15:28:20 +0700

    user_resgister_login_logout

--- Ecommerce/shoppingsite/urls.py
+++ Ecommerce/shoppingsite/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
+    path('register/', views.register, name='user-register'),
+    path('profile/', views.profile, name='user-profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
 ]
